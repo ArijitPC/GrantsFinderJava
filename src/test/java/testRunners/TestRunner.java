a510999a6975b9c6ff0fc6be5c60124ef4351ed8
ArijitPC <a_p_chowdhury@hotmail.com>
2024-09-14 03:10:20 +1000

    adding testng.xml

--- src/test/java/testRunners/TestRunner.java
+++ src/test/java/testRunners/TestRunner.java
@@ -1,14 +1,19 @@
 package testRunners;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
-@RunWith(Cucumber.class)
+
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepDefinitions"},
         plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
-
